9dae947c66a839bf58c1ea9b144efa8fcf168e93
coder-binary <noman.g341@gmail.com>
2022-02-24 22:40:14 +0600

    initial commit

---
+++ vidoe.js
@@ -0,0 +1,6 @@
+// document.getElementById('video').style.display = 'none';
+const videos = document.getElementsByTagName('video');
+for (const video of videos) {
+    // video.classList.add('video-js');
+    // video.removeAttribute('controls');
+};
