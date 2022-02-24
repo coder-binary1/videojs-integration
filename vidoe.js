bc9b80f9c18ae1a10de10c0c98d9dd5091541ae5
coder-binary <noman.g341@gmail.com>
2022-02-25 01:19:34 +0600

    remove html-5 player & integrate videojs player

--- vidoe.js
+++ vidoe.js
@@ -1,6 +1,10 @@
-// document.getElementById('video').style.display = 'none';
 const videos = document.getElementsByTagName('video');
 for (const video of videos) {
-    // video.classList.add('video-js');
-    // video.removeAttribute('controls');
-};
+    const videoLocation = video.getAttribute('src');
+    video.classList.add('video-js');
+    video.setAttribute('data-setup', '{}');
+    video.removeAttribute('src');
+    const videoSource = document.createElement('source');
+    videoSource.setAttribute('src', videoLocation)
+    video.appendChild(videoSource);
+}
